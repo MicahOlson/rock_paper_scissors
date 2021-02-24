5bcc6c3a92155af020590e3a13c7e41e425395c9
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-24 11:43:43 -0800

    Add red-stage good fail spec for scissors loses to rock
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- spec/rock_paper_spec.rb
+++ spec/rock_paper_spec.rb
@@ -3,8 +3,14 @@ require('pry')
 require('rock_paper')
 
 describe('RPS#p1_wins?') do
-  it("returns true if rock is the object and scissors is the argument") do
+  it("returns true if player one plays rock and player two plays scissors") do
     game = RPS.new()
     expect(game.p1_wins?("rock", "scissors")).to(eq(true))
   end
+
+  it("returns false if player one plays scissors and player two plays rock") do
+    game = RPS.new()
+    expect(game.p1_wins?("scissors", "rock")).to(eq(false))
+  end
 end
+
