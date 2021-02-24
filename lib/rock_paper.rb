c45f637fea5038994ae742d3b231c4e42adc8b05
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-24 13:21:09 -0800

    Add code in model to pass paper beats rock
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- lib/rock_paper.rb
+++ lib/rock_paper.rb
@@ -1,6 +1,6 @@
 class RPS
   def p1_wins?(p1_choice, p2_choice)
-    if p1_choice == "rock" && p2_choice == "scissors"
+    if p1_choice == "rock" && p2_choice == "scissors" || p1_choice == "paper" && p2_choice == "rock"
       true
     else
       false
