79097899e41686c3e0bcbe60bab729143e3e8bf7
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-24 13:31:34 -0800

    Add code in model to pass for tie
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- lib/rock_paper.rb
+++ lib/rock_paper.rb
@@ -2,6 +2,8 @@ class RPS
   def p1_wins?(p1_choice, p2_choice)
     if p1_choice == "rock" && p2_choice == "scissors" || p1_choice == "paper" && p2_choice == "rock" || p1_choice == "scissors" && p2_choice == "paper"
       true
+    elsif p1_choice == p2_choice
+      "tie"
     else
       false
     end
