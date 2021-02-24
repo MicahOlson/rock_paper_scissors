d8a1ac8f105c3aad956d69e37454daa28112fccb
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-24 11:12:05 -0800

    Add red-stage spec for rock beats scissors with good fail
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

---
+++ lib/rock_paper.rb
@@ -0,0 +1,4 @@
+class RPS
+  def p1_wins?(p1_choice, p2_choice)
+  end
+end
--- spec/rock_paper_spec.rb
+++ spec/rock_paper_spec.rb
@@ -1,9 +1,10 @@
 require('rspec')
 require('pry')
-require('rps')
+require('rock_paper')
 
-describe('#rps') do
-  it("") do
-    expect(rps("")).to(eq(""))
+describe('RPS#p1_wins?') do
+  it("returns true if rock is the object and scissors is the argument") do
+    game = RPS.new()
+    expect(game.p1_wins?("rock", "scissors")).to(eq(true))
   end
 end
